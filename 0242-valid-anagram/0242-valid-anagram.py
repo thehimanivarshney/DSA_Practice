d6e70dea033e656628984fb81a791982467865d4
Himani Varshney <himanivarshney42@gmail.com>
2026-06-22 22:19:44 +0530

    Time: 19 ms (64.93%), Space: 14.3 MB (13.02%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        for i in range (len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        for val in count:
+            if val != 0:
+                return False
+        return True        
